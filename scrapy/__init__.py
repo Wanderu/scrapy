5245a8d531cbc14895d64b7768926d08a930b8c8
mwarren <mwarren@wanderu.com>
2019-01-22 15:08:57 -0500

    version bump, just to be extra sure

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -2,8 +2,8 @@
 Scrapy - a screen scraping framework written in Python
 """
 
-version_info = (0, 16, 4)
-__version__ = "0.16.4"
+version_info = (0, 16, 6)
+__version__ = "0.16.6"
 
 import sys, os, warnings
 
